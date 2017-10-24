fa48f41a855673a94a8f030e5506d25228e0630f
kuujinbo <kuujinbo@gmail.com>
2017-10-23 19:46:19 -0700

    AssemblyInfo

--- src/kuujinbo.Mvc.NET.W3CLog/Properties/AssemblyInfo.cs
+++ src/kuujinbo.Mvc.NET.W3CLog/Properties/AssemblyInfo.cs
@@ -1,17 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("kuujinbo.Mvc.NET.W3CLog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A dead-simple, non-adaptable, single-purpose thin wrapper for NLog that logs using WC3 Extended Log File Format recommendations.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("kuujinbo.Mvc.NET.W3CLog")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -29,8 +22,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
